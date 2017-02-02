d9dafb4fc7572de5ac214f6526d5f220cef6a60a
luka1983 <luka.miljak@gmail.com>
2017-02-02 23:43:42 +0100

    variardic formated serial print

--- src/serial.h
+++ src/serial.h
@@ -1,7 +1,6 @@
 #include <avr/io.h>
-#include <string.h>
 #include <avr/interrupt.h>
 
 void init_serial(uint16_t brate, uint8_t dbits, uint8_t parity, uint8_t sbits);
 void sputc(const char c);
-void sprints(const char* str);
+void sprints(const char* fmt, ...);
