6e69b93f02ab0d4d032797d1e4bb2c47054e5018
Matija Glavinic Pecotic <matija.glavinic-pecotic.ext@nokia.com>
2017-02-14 21:16:27 +0100

    Initial version of beermon service
    beermon service is in charge of handling incoming messages and
    storing them where web server can process them

---
+++ server_software/apps/beermon_handler.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import syslog
+import time
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import datetime
+import threading
+import subprocess
+import os.path
+from os import listdir
+from datetime import timedelta
+from datetime import datetime
+import sys
+import re
+
+# Local mqtt broker address
+MQTT_HOST="localhost"
+MQTT_HOST_PORT=1883
+
+RESULT_DIR="/var/www/html/beer/results/"
+
+def process_message(topic, text):
+    print("Processing message")
+    sensor = topic.split('/')[-1]
+    date_value_pair = text.split(',')
+    if date_value_pair[0] is not None:
+        # Check whether date is formatted as date
+        date_pattern = re.compile("2[0-9]{3}-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]")
+        if date_pattern.match(date_value_pair[0]):
+            date = date_value_pair[0]
+            print("Date pattern matches! " + date)
+        else:
+            print("Pattern on date doesnt match");
+            return
+    if date_value_pair[1] is not None:
+        # Check that value matches decimal number
+        value_pattern = re.compile("[\d\.\d]+")
+        if value_pattern.match(date_value_pair[1]):
+            value = date_value_pair[1];
+            print("Value pattern matches! " + value);
+        else:
+            print("Pattern on value doesnt match");
+            return
+    with open(RESULT_DIR + sensor, "a") as myfile:
+        myfile.write(text)
+
+def beermon_handler_on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("beermon/#")
+
+def beermon_handler_on_message(client, userdata, msg):
+    text = msg.payload.decode(encoding="utf-8", errors="ignore")
+    print("Got message, topic <%s>, text <%s>" %(msg.topic, text))
+    process_message(msg.topic, text)
+
+if __name__ == "__main__":
+
+    print("Starting beermon handler")
+    beermon_handler = mqtt.Client()
+    beermon_handler.on_connect = beermon_handler_on_connect
+    beermon_handler.on_message = beermon_handler_on_message
+    beermon_handler.connect(MQTT_HOST, MQTT_HOST_PORT, 60)
+    beermon_handler.loop_start()
+
+    while True:
+        print("Alive... ")
+        time.sleep(600)
