e61ed8c9b50148b548ca90dfc68afdd18506e51f
Matija Glavinic Pecotic <matija.glavinic-pecotic.ext@nokia.com>
2019-01-23 09:31:41 +0100

    sensors: Change pins where temperature sensors are connected
    PORTD is needed for LCD. Move temperature sensors to
    PC0 (t1 - control input) and PC1 (t2 - free sensor).

--- firmware/src/temperature_channel_config.h
+++ firmware/src/temperature_channel_config.h
@@ -21,10 +21,10 @@ struct temperature_channel channels[] = {
 		.name = "t1",
 		.sensor = temperature_sensor_ds18b20,
 		.result_multiplier = DS18B20_MUL,
-		.port = &PORTD,
-		.direction = &DDRD,
-		.portin = &PIND,
-		.pin_mask = 1 << 6,
+		.port = &PORTC,
+		.direction = &DDRC,
+		.portin = &PINC,
+		.pin_mask = 1 << 0,
 		.temp_raw = 0,
 		.temp_c = 0
 	},
@@ -32,23 +32,12 @@ struct temperature_channel channels[] = {
 		.name = "t2",
 		.sensor = temperature_sensor_ds18b20,
 		.result_multiplier = DS18B20_MUL,
-		.port = &PORTD,
-		.direction = &DDRD,
-		.portin = &PIND,
-		.pin_mask = 1 << 5,
+		.port = &PORTC,
+		.direction = &DDRC,
+		.portin = &PINC,
+		.pin_mask = 1 << 1,
 		.temp_raw = 0,
 		.temp_c = 0
 	},
-	// {
-	// 	.name = "t3",
-	// 	.sensor = temperature_sensor_ds18b20,
-	// 	.result_multiplier = DS18B20_MUL,
-	// 	.ds18b20_port = &PORTD,
-	// 	.ds18b20_direction = &DDRD,
-	// 	.ds18b20_portin = &PIND,
-	// 	.pin_mask = 1 << 4,
-	// 	.temp_raw = 0,
-	// 	.temp_c = 0
-	// },
 };
 #endif
