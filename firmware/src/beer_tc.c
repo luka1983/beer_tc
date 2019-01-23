37d12367ff2e6c62b2cbc72a8209dc6718891adf
Matija Glavinic Pecotic <matija.glavinic-pecotic.ext@nokia.com>
2019-01-23 10:22:15 +0100

    lcd: Add 1st version of using LCD
    This for the moment crude solution, but it will be useful
    for future development. Ts would ideally be updated where
    it is set, and Tc update where read. It will be sorted out
    later.

--- firmware/src/beer_tc.c
+++ firmware/src/beer_tc.c
@@ -2,6 +2,7 @@
 #include <avr/interrupt.h>
 #include <string.h>
 #include <util/delay.h>
+#include <stdlib.h>
 #include "tcontrol.h"
 #include "serial.h"
 #include "commands.h"
@@ -24,12 +25,46 @@ void get_id(char* id) {
 	return;
 }
 
+void lcd_update_tc(void)
+{
+	int err = 0;
+	double temp_double = 0;
+	char string_temperature[12];
+	struct temperature_channel *tc = temperature_get_channel_by_name("t1");
+	err = temperature_read(tc, 0);
+	if (err != 0) {
+		strcpy(string_temperature, "ERROR");
+	} else {
+		temp_double = (double)tc->temp_raw/(double)tc->result_multiplier;
+		dtostrf(temp_double, 2, 4, string_temperature);
+	}
+	lcd_gotoxy(0,0);
+	lcd_puts("Tc: ");
+	lcd_puts(string_temperature);
+}
+
+void lcd_update_ts(void)
+{
+	int err = 0;
+	int ts = get_ts();
+	double temp_double = 0;
+	char string_temperature[12];
+	temp_double = (double)ts/(double)DEC_DIV;
+	dtostrf(temp_double, 2, 4, string_temperature);
+	lcd_gotoxy(0,1);
+	lcd_puts("Ts: ");
+	lcd_puts(string_temperature);
+}
+
 int main(void) {
 	sei();			// Enable interrupts
 	init_control_loop(1000);
 	init_serial(19200, 8, 0, 1);
 
 	set_command_handler(GetId, &get_id);
+
+	lcd_init(LCD_DISP_ON);
+	lcd_clrscr();
 
 	start_control_loop();
 
@@ -40,6 +75,9 @@ int main(void) {
 #ifdef LCD_DEMO
 		lcd_demo();
 #endif
+		lcd_update_tc();
+		lcd_update_ts();
+		_delay_ms(1000);
 	}
 
 	return 0;
